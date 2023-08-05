7f763fd7cce5da7509208bb2fbb01d2b1570c28c
Salman Ghouri <116343712+salmanghouridev@users.noreply.github.com>
2023-08-05 11:20:01 +0000

    defeqqw

---
+++ cvbrtea/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+   <>
+      <div>
+      <Button>Click me</Button>
+    </div>
+   </>
+  )
+}
