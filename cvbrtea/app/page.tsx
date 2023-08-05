f9e434113b5caa43dfd4e4afd6ae450dea103e70
Salman Ghouri <116343712+salmanghouridev@users.noreply.github.com>
2023-08-05 11:55:41 +0000

    fknekafn

--- cvbrtea/app/page.tsx
+++ cvbrtea/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
+import Navbar from './Navbar/page'
+import Banner from './Banner/page'
 
 export default function Home() {
   return (
    <>
       <div>
-      <Button>Click me</Button>
+<Navbar/>
+<Banner/>
     </div>
    </>
   )
