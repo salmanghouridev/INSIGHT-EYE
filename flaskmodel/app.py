850b93128fbf0fc8ec5df633ca39b84d66ee756c
Salman Ghouri <116343712+salmanghouridev@users.noreply.github.com>
2023-09-12 09:56:28 +0000

    dade

--- flaskmodel/app.py
+++ flaskmodel/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import cv2
-import cvzone
-from cvzone.FaceMeshModule import FaceMeshDetector
 import numpy as np
+import dlib
 
 app = Flask(__name__)
 
-# Initialize the detector
-detector = FaceMeshDetector(maxFaces=1)
+# Load the pre-trained model
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+detector = dlib.get_frontal_face_detector()
 
 # Global variables to store Snellen chart words and current index
 snellen_chart = ["E", "F", "P", "T", "O", "L", "C", "D", "Z", "V"]
@@ -35,23 +35,26 @@ def verify_word():
 @app.route('/process_image', methods=['POST'])
 def process_image():
     img_data = request.files['image'].read()
-    img = cv2.imdecode(np.fromstring(img_data, np.uint8), cv2.IMREAD_UNCHANGED)
+    img = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_UNCHANGED)
 
-    # Your existing code for processing the image goes here...
-    img, faces = detector.findFaceMesh(img, draw=False)
-    
+    # detect faces in the image
+    faces = detector(img)
+
     response_data = {}
     if faces:
-        face = faces[0]
-        pointLeft = face[145]
-        pointRight = face[374]
-        
-        w, _ = detector.findDistance(pointLeft, pointRight)
-        W = 6.3
-        f = 590
+        # Get landmarks of the first face detected
+        landmarks = predictor(img, faces[0])
+
+        # Calculate the distance between two eye landmarks (for instance, points 36 and 45)
+        pointLeft = (landmarks.part(36).x, landmarks.part(36).y)
+        pointRight = (landmarks.part(45).x, landmarks.part(45).y)
+
+        w = ((pointRight[0] - pointLeft[0])**2 + (pointRight[1] - pointLeft[1])**2) ** 0.5
+        W = 6.3  # average pupillary distance in cm
+        f = 590  # focal length of the webcam (you may need to adjust this)
         d_cm = (W * f) / w
         d_inch = d_cm / 2.54
-        
+
         response_data = {
             'distance': int(d_inch)
         }
