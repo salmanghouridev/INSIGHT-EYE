232df034943d4fad79971663a0a963c4ed014415
Muhammad Azam Mustafa <130210100+mazammustafa@users.noreply.github.com>
2023-09-25 23:51:29 +0500

    Strabismus Update

--- midyearmodel/strabismus.py
+++ midyearmodel/strabismus.py
@@ -4,6 +4,7 @@ import dlib
 import math
 import os
 import time
+from flask import flash, redirect, url_for
 from tkinter import messagebox
 
 strabismus = Blueprint('strabismus', __name__)
@@ -72,19 +73,39 @@ class RealTimeStrabismusDetectionApp:
     def calculate_vertical_displacement(self, left_eye_center, right_eye_center):
         return left_eye_center[1] - right_eye_center[1]
 
+# def predict_strabismus_type(self, angle, vertical_displacement):
+#         if abs(angle) > 8:
+#             if angle > 8:
+#                 return "Esotropia"
+#             elif angle < -8:
+#                 return "Exotropia"
+#         elif abs(vertical_displacement) > 5:
+#             if vertical_displacement > 5:
+#                 return "Hypertropia"
+#             elif vertical_displacement < -5:
+#                 return "Hypotropia"
+#         else:
+#             return "None"
+
     def predict_strabismus_type(self, angle, vertical_displacement):
-        if abs(angle) > 8:
-            if angle > 8:
-                return "Esotropia"
-            elif angle < -8:
-                return "Exotropia"
-        elif abs(vertical_displacement) > 5:
-            if vertical_displacement > 5:
-                return "Hypertropia"
-            elif vertical_displacement < -5:
-                return "Hypotropia"
+        # Thresholds for diagnosing strabismus based on angle and vertical displacement
+        angle_threshold = 8  # degrees
+        displacement_threshold = 5  # millimeters or degrees
+
+        if abs(angle) > angle_threshold:
+            if angle > 0:
+                return "Esotropia"  # Inward deviation
+            elif angle < 0:
+                return "Exotropia"  # Outward deviation
+
+        elif abs(vertical_displacement) > displacement_threshold:
+            if vertical_displacement > 0:
+                return "Hypertropia"  # Upward displacement
+            elif vertical_displacement < 0:
+                return "Hypotropia"  # Downward displacement
         else:
-            return "None"
+            return "None"  # No significant strabismus detected
+
 
     def generate_frames(self):
         while True:
@@ -110,7 +131,7 @@ class RealTimeStrabismusDetectionApp:
             frame_with_points, report = self.process_image(frame)
 
             # Specify the directory path where you want to save the files
-            base_dir = r"D:\FYP AZAM\Cap"  # Use a raw string to handle backslashes
+            base_dir = r"D:\Projects\Final Year Project\Flask 2 - Copy\Cap"  # Use a raw string to handle backslashes
 
             # Find the existing report folders and determine the next folder name
             existing_folders = [f for f in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, f))]
@@ -137,15 +158,35 @@ class RealTimeStrabismusDetectionApp:
             # Show a message box to confirm the capture
             messagebox.showinfo("Capture", f"Frame with points and lines and report saved in '{report_folder}'")
 
+            # Shut down the camera
+            self.shutdown_camera()
+
+            # flash(f"Frame with points and lines and report saved in '{report_folder}'", 'success')
+
+            # Redirect to the dashboard
+            return redirect(url_for('dashboard.index2'))
+
         except Exception as e:
-            messagebox.showerror("Error", str(e))
+            flash(f"Error: {str(e)}", 'error')
+            return redirect(url_for('dashboard.html'))
+
+    def shutdown_camera(self):
+        if self.video_capture.isOpened():
+            self.video_capture.release()
+
 
 def video_feed():
-    app.detector_app = RealTimeStrabismusDetectionApp()
-    return Response(app.detector_app.generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    strabismus.detector_app = RealTimeStrabismusDetectionApp()
+    return Response(strabismus.detector_app.generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @strabismus.route('/')
 def index2():
     return render_template('astrabismus.html')
+
+@strabismus.route('/startingpage_Strabismus')
+def indexlanding():
+    return render_template('strabismus2.html')
+
 
 @strabismus.route('/video_feeds')
 def video_feed_route():
@@ -178,11 +219,12 @@ def shutdown_video_feed():
     if hasattr(strabismus, 'detector_app'):
         strabismus.detector_app.video_capture.release()
 
-def shutdown_server():
-    # Perform any cleanup or shutdown tasks here
-    print("Shutting down the server gracefully")
-    os._exit(0)  # Exit the application
+# def shutdown_server():
+#     # Shut down the camera
+#     if hasattr(strabismus, 'detector_app'):
+#         strabismus.detector_app.shutdown_camera()
 
-def video_feed():
-    strabismus.detector_app = RealTimeStrabismusDetectionApp()
-    return Response(strabismus.detector_app.generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+#     # Perform any other cleanup or shutdown tasks here
+#     print("Shutting down the server gracefully")
+#     os._exit(0)  # Exit the application
+
